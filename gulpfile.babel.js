5d9d6187c6ebc3e3034664a2c599dae108c5ad6e
Phil Lennon <phil@frontendphil.com>
2016-04-14 12:32:02 +0100

    integrate jasmine into dev and deploy tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,23 +12,18 @@ const tasks = requireDir(__dirname + '/tasks') // eslint-disable-line
 // Watch task
 gulp.task('watch', () => {
   gulp.watch(config.paths.scss + '**/*.scss', ['scss'])
-  gulp.watch(config.paths.js + '**/*.js', ['webpack'])
+  gulp.watch(config.paths.spec + '**/*[Ss]pec.js', ['jasmine'])
+  gulp.watch(config.paths.js + '**/*.js', ['jasmine', 'webpack'])
   gulp.watch(config.paths.img + '{,**/}*.{png,jpg,gif,svg}', ['img'])
   gulp.watch(config.paths.icons + '**/*.svg', ['svgsprite'])
   gulp.watch([config.paths.fonts + '**/*', config.paths.files + '**/*'], ['copy'])
   gulp.watch([config.paths.layouts + '**/*.hbs', config.paths.pages + '**/*.hbs', config.paths.partials + '**/*.hbs'], ['metalsmith'])
 })
 
-// JS Dev Watch task
-gulp.task('dev-watch', () => {
-  gulp.watch('spec/**/*[Ss]pec.js', ['run-jasmine'])
-  gulp.watch(config.paths.js + '**/*.js', ['run-jasmine'])
-})
-
 // Build website, either with development or minified assets and run server with live reloading
 gulp.task('default', callback => {
   runSequence(
-    'run-jasmine',
+    'jasmine',
     'clean',
     'metalsmith',
     ['htmlmin', 'svgsprite', 'scss', 'webpack', 'img', 'copy'],
@@ -37,19 +32,10 @@ gulp.task('default', callback => {
   )
 })
 
-// Run tests and watch js/spec files
-gulp.task('dev', callback => {
-  runSequence(
-    'run-jasmine',
-    'dev-watch',
-    callback
-  )
-})
-
 // Build website, either with development or minified assets depending on flag
 gulp.task('deploy', callback => {
   runSequence(
-    'run-jasmine',
+    'jasmine',
     'clean',
     'metalsmith',
     ['htmlmin', 'svgsprite', 'scss', 'webpack', 'img', 'copy'],
@@ -58,7 +44,7 @@ gulp.task('deploy', callback => {
   )
 })
 
-// Run the audit task to check the code
+// Run the audit task to check code standards
 gulp.task('auditcode', callback => {
   runSequence(
     'scsslint',
@@ -66,25 +52,3 @@ gulp.task('auditcode', callback => {
     callback
   )
 })
-
-// Run the audit task to check the built website for accessibility
-// NOTE: Not used yet
-/*
-gulp.task('auditsite', callback => {
-  runSequence(
-    'deploy',
-    callback
-  )
-})
-*/
-
-// Run the audit task to check performance using ...
-// NOTE: Not used yet
-/*
-gulp.task('auditperf', callback => {
-  runSequence(
-    'deploy',
-    callback
-  )
-})
-*/
