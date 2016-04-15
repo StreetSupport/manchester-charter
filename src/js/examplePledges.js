0ba5868c4010b713696e5894f06e439043fae549
vince (polyhat) <vslee888@gmail.com>
2016-04-15 17:08:03 +0100

    hard-code example pledges

---
+++ src/js/examplePledges.js
@@ -0,0 +1,77 @@
+export function getSupporterCategories() {
+  return [{
+    'name': 'A Person with experience of homelessness',
+    'listOrder': 1,
+    'examplePledges': [
+      'Join an action group to design services.',
+      'Volunteer time to help a local organisation.',
+      'Share your story to inform others about homelessness.',
+      'Become an ambassador for the charter.',
+      'Give feedback on experiences and services.'
+    ]
+  }, {
+    'name': 'A Business',
+    'listOrder': 1,
+    'examplePledges': [
+      'Offer employment opportunities by providing entry level jobs.',
+      'Offer your skills and resources to local organisations.',
+      'Inform staff about homelessness and alternative giving.',
+      'Raise money for local organisations.',
+      'Donate to the Big Change campaign.',
+      'Help with marketing to raise awareness of the charter.',
+      'Make it easier to open a bank account.',
+      'Offer facilities to homeless people e.g. toilets, drinking water.'
+    ]
+  }, {
+    'name': 'A Service Provider',
+    'listOrder': 1,
+    'examplePledges': [
+      'Review your services in line with the charter vision and values.',
+      'Bring representatives to relevant action groups.',
+      'Ensure your staff sign up to the values of the charter,',
+      'Include service users in the design your services.',
+      'Regularly invite feedback and listen to your service users.',
+      'Provide out of hours dropins and services.',
+      'Provide more sit ups.',
+      'Increase turn around time in hostels so more rooms are available.',
+      'Commit to a \'no wrong door\' policy.'
+    ]
+  }, {
+    'name': 'A member of the Public Sector',
+    'listOrder': 1,
+    'examplePledges': [
+      'Ensure your facilities are accessible to people who are homeless.',
+      'Treat people in line with the values in the charter.',
+      'Train your staff on the charter and the rights of homeless people.',
+      'Provide training opportunities.',
+      'Ensure key decision makers visit services.',
+      'Include homeless people in meetings.'
+    ]
+  }, {
+    'name': 'A member of a community or faith-based groups',
+    'listOrder': 1,
+    'examplePledges': [
+      'Provide emergency accomodation for rough sleepers all year round.',
+      'Inform your community about homelessness and altenative giving.',
+      'Domate or fundraise for local organisations.',
+      'Provide housing - supported housing or host families.'
+    ]
+  }, {
+    'name': 'A person from an educational organisation',
+    'listOrder': 1,
+    'examplePledges': [
+      'Make a donation to Big Change Manchester.',
+      'Develop staff and students homelessness ambassadors.',
+      'Provide student placements to increase capacity and innovation.',
+      'Develop work placements and employment opportunities.',
+      'Provide research capacity to evaluate the impact of the charter.'
+    ]
+  }, {
+    'name': 'An individual',
+    'listOrder': 1,
+    'examplePledges': [
+      'Give time/money/items based on what is needed',
+      'Improve understanding of homelessness.'
+    ]
+  }]
+}
