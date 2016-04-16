b6449c8b327d52e36102872793ea32d98f1e6a25
Vince Lee <vslee888@gmail.com>
2016-04-16 09:52:26 +0100

    bin off redundant guff

--- src/js/templating.js
+++
@@ -1,8 +0,0 @@
-var Hogan = require('hogan.js')
-
-export function render (templateId, data, output, callback) {
-  var theTemplate = document.getElementById(templateId).innerHTML
-  var compileTemplate = Hogan.compile(theTemplate)
-  document.getElementById(output).innerHTML = compileTemplate.render(data)
-  callback()
-}
