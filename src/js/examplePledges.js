470793d80c7b366195b24dce3cc2b9c4d16cc80e
Viv Slack <viv@objectivesfirst.com>
2016-04-19 14:39:49 +0100

    Moved order of pledges back to examples first.

--- src/js/examplePledges.js
+++ src/js/examplePledges.js
@@ -17,7 +17,7 @@ export function getSupporterCategories () {
       'Making a donation to Big Change Manchester.',
       'Registering as a volunteer on Street Support',
       'Asking my employer to pledge to support the Charter',
-      'Becoming more informed about homelessness, and what services are available',
+      'Learning what services are available and how i can help',
       'Including my name on the charter supporters database.'
     ]
   },{
