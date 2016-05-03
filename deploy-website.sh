d8acbe17848773bdb19288d2d496648e7bf7f5ac
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2016-05-03 12:56:12 +0100

    if dev/uat disallow robots

--- deploy-website.sh
+++ deploy-website.sh
@@ -23,6 +23,24 @@ if [[ $TRAVIS_BRANCH == 'release' ]]
     AZURE_WEBSITE=$PROD_AZURE_WEBSITE_W_EUR
 fi
 
+# block robots if not live
+if [[ $TRAVIS_BRANCH == 'develop' ]] || [[ $TRAVIS_BRANCH == 'uat' ]]
+  then
+    cd src/files
+    rm robots.txt
+    cat > robots.txt << EOF
+    User-agent: *
+    Disallow: /
+    EOF
+
+    echo "robots.txt rewritten to:"
+    cat robots.txt
+
+    cd ../../
+fi
+
+
+
 # Get the commit details
 THE_COMMIT=`git rev-parse HEAD`
 
