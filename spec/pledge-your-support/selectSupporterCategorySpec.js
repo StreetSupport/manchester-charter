b85c5d1fe7d2d38d0ed48b711e96ac2e08f43356
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2016-05-05 13:09:27 +0100

    scroll to top of supporter category accordion panel

--- spec/pledge-your-support/selectSupporterCategorySpec.js
+++ spec/pledge-your-support/selectSupporterCategorySpec.js
@@ -10,21 +10,34 @@ var browser = require('../../src/js/browser')
 
 describe('Pledge Your Support - Select supporter category', () => {
   var sut
+  var browserJumpToStub
 
   beforeEach(() => {
-    sinon.stub(browser, 'scrollTo')
+    browserJumpToStub = sinon.stub(browser, 'jumpTo')
     sut = new Model()
-    sut.accordionOpened({ childNodes: [
-      'text',
-      { innerHTML: 'supporter category' }
-    ] }, { })
+    sut.accordionOpened({
+      childNodes: [
+        'text',
+        {
+          innerHTML: 'supporter category',
+          parentNode: {
+            id: 'kaplaah!'
+          }
+        }
+      ],
+
+    }, { })
   })
 
   afterEach(() => {
-    browser.scrollTo.restore()
+    browser.jumpTo.restore()
   })
 
   it('- Should set supporter category', () => {
     expect(sut.formModel().supporterCategory()).toEqual('supporter category')
+  })
+
+  it('- Should jump to selected category', () => {
+    expect(browserJumpToStub.withArgs('#kaplaah!').calledOnce).toBeTruthy()
   })
 })
